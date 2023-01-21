699e29aa91dce5fe3de435bf54da7655848d1bbe
Eduard Marrugo <eduarmarrugocm@unimagdalena.edu.co>
2023-01-20 22:48:06 -0500

    Updates

---
+++ precache-manifest.11f0c588da14fe47a099e98fb8724931.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8d9fe81f1b2918ef9e0",
+    "url": "/curso-intro-react/static/css/main.f2dc0784.chunk.css"
+  },
+  {
+    "revision": "c8d9fe81f1b2918ef9e0",
+    "url": "/curso-intro-react/static/js/main.c8d9fe81.chunk.js"
+  },
+  {
+    "revision": "480a353721bd092f9455",
+    "url": "/curso-intro-react/static/js/1.480a3537.chunk.js"
+  },
+  {
+    "revision": "8f31ac8995903ac72b62",
+    "url": "/curso-intro-react/static/js/runtime~main.8f31ac89.js"
+  },
+  {
+    "revision": "3f45f056930db93c519b5c0b69cd2407",
+    "url": "/curso-intro-react/index.html"
+  }
+];
